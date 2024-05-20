8fa9b46ee0c2b11ac912c89365b999411ff22d97
invinc1ble7 <93664486+invinc1ble7@users.noreply.github.com>
2024-05-20 14:38:58 +0200

    Create ubuntu.sh

---
+++ ubuntu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+pkg install proot proot-distro -y && pd install ubuntu-lts
+echo 'cd ~ && pd login ubuntu-lts' > ../usr/bin/ubuntu
+chmod +x ../usr/bin/ubuntu
+proot-distro login ubuntu-lts -- apt update -y
+proot-distro login ubuntu-lts -- apt upgrade -y
+proot-distro login ubuntu-lts -- apt install vncserver -y
+proot-distro login ubuntu-lts -- apt install xfce4 -y
+proot-distro login ubuntu-lts -- apt install xfce4-goodies -y
+cd ~ && rm-rf Ubuntu-in-Termux
