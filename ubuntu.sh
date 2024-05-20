355fc314e69f19c12e953729d22eedd99c4e36fb
invinc1ble7 <93664486+invinc1ble7@users.noreply.github.com>
2024-05-20 14:57:02 +0200

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -4,7 +4,7 @@ echo 'cd ~ && pd login ubuntu-lts' > ../usr/bin/ubuntu
 chmod +x ../usr/bin/ubuntu
 proot-distro login ubuntu-lts -- apt update -y
 proot-distro login ubuntu-lts -- apt upgrade -y
-proot-distro login ubuntu-lts -- apt install vncserver -y
+proot-distro login ubuntu-lts -- apt install tightvncserver -y
 proot-distro login ubuntu-lts -- apt install xfce4 -y
 proot-distro login ubuntu-lts -- apt install xfce4-goodies -y
 cd ~ && rm -rf Ubuntu-in-Termux
