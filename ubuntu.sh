79ef266a853c5765be4094263b1f76da9d2cbd15
invinc1ble7 <93664486+invinc1ble7@users.noreply.github.com>
2024-05-21 11:29:52 +0200

    Aktualisieren von ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-pkg install proot proot-distro -y && pd install ubuntu
-echo 'cd ~ && pd login ubuntu' > ../usr/bin/ubuntu
+pkg install proot proot-distro -y && pd install ubuntu-oldlts
+echo 'cd ~ && pd login ubuntu-oldlts' > ../usr/bin/ubuntu
 chmod +x ../usr/bin/ubuntu
-proot-distro login ubuntu -- apt update -y
-proot-distro login ubuntu -- apt upgrade -y
-proot-distro login ubuntu -- apt install xfce4 -y
-proot-distro login ubuntu -- apt install xfce4-goodies -y
-proot-distro login ubuntu -- apt install tightvncserver -y
+proot-distro login ubuntu-oldlts -- apt update -y
+proot-distro login ubuntu-oldlts -- apt upgrade -y
+proot-distro login ubuntu-oldlts -- apt install xfce4 -y
+proot-distro login ubuntu-oldlts -- apt install xfce4-goodies -y
+proot-distro login ubuntu-oldlts -- apt install tightvncserver -y
 cd ~ && rm -rf Ubuntu-in-Termux
