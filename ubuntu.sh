e345534d47f29c9aee535490f410d1f0ca0de870
invinc1ble7 <93664486+invinc1ble7@users.noreply.github.com>
2024-05-21 19:54:01 +0200

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -2,6 +2,6 @@
 pkg install proot proot-distro -y && pd install ubuntu
 echo 'cd ~ && pd login ubuntu' > ../usr/bin/ubuntu
 chmod +x ../usr/bin/ubuntu
-proot-distro login ubuntu-oldlts -- apt update -y
-proot-distro login ubuntu-oldlts -- apt upgrade -y
+proot-distro login ubuntu -- apt update -y
+proot-distro login ubuntu -- apt upgrade -y
 cd ~ && rm -rf Ubuntu-in-Termux
