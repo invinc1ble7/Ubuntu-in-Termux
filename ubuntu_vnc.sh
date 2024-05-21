6f95792e2799403448ee6fc3b69809278abdfb25
invinc1ble7 <93664486+invinc1ble7@users.noreply.github.com>
2024-05-21 23:26:29 +0200

    Update ubuntu_vnc.sh

--- ubuntu_vnc.sh
+++ ubuntu_vnc.sh
@@ -4,6 +4,7 @@ echo 'cd ~ && pd login ubuntu-oldlts' > ../usr/bin/ubuntu
 chmod +x ../usr/bin/ubuntu
 proot-distro login ubuntu-oldlts -- apt update -y
 proot-distro login ubuntu-oldlts -- apt upgrade -y
+proot-distro login ubuntu-oldlts -- apt install sudo nano
 proot-distro login ubuntu-oldlts -- apt install xfce4 -y
 proot-distro login ubuntu-oldlts -- apt install xfce4-goodies -y
 proot-distro login ubuntu-oldlts -- apt install tightvncserver -y
