21e28e3626536f036bdc29e0c21a0e081d85549d
invinc1ble7 <93664486+invinc1ble7@users.noreply.github.com>
2024-05-21 23:25:51 +0200

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -4,4 +4,5 @@ echo 'cd ~ && pd login ubuntu' > ../usr/bin/ubuntu
 chmod +x ../usr/bin/ubuntu
 proot-distro login ubuntu -- apt update -y
 proot-distro login ubuntu -- apt upgrade -y
+proot-distro login ubuntu -- apt install sudo nano adduser
 cd ~ && rm -rf Ubuntu-in-Termux
