6a61679c67cb831b49c3330493b50f2548d41afe
invinc1ble7 <93664486+invinc1ble7@users.noreply.github.com>
2024-05-23 23:17:22 +0200

    Update ubuntu_vnc.sh

--- ubuntu_vnc.sh
+++ ubuntu_vnc.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-pkg install proot proot-distro -y && pd install ubuntu-oldlts
+pkg install proot-distro -y && pd install ubuntu-oldlts
 echo 'cd ~ && pd login ubuntu-oldlts' > ../usr/bin/ubuntu
 chmod +x ../usr/bin/ubuntu
 proot-distro login ubuntu-oldlts -- apt update -y
 proot-distro login ubuntu-oldlts -- apt upgrade -y
-proot-distro login ubuntu-oldlts -- apt install sudo nano
+proot-distro login ubuntu-oldlts -- apt install sudo nano -y
 proot-distro login ubuntu-oldlts -- apt install xfce4 -y
 proot-distro login ubuntu-oldlts -- apt install xfce4-goodies -y
 proot-distro login ubuntu-oldlts -- apt install tigervnc-standalone-server -y
