6781e3322f961f297a83968ebbe317c8d0aba8c9
invinc1ble7 <93664486+invinc1ble7@users.noreply.github.com>
2024-05-23 22:31:00 +0200

    Aktualisieren von ubuntu_vnc.sh

--- ubuntu_vnc.sh
+++ ubuntu_vnc.sh
@@ -7,5 +7,5 @@ proot-distro login ubuntu-oldlts -- apt upgrade -y
 proot-distro login ubuntu-oldlts -- apt install sudo nano
 proot-distro login ubuntu-oldlts -- apt install xfce4 -y
 proot-distro login ubuntu-oldlts -- apt install xfce4-goodies -y
-proot-distro login ubuntu-oldlts -- apt install tightvncserver -y
+proot-distro login ubuntu-oldlts -- apt install tigervnc-standalone-server -y
 cd ~ && rm -rf Ubuntu-in-Termux
